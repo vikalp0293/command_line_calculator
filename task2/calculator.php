58b6a36fbbd23471faf80e23632912f89e285181
vikalp0293 <31693175+vikalp0293@users.noreply.github.com>
2019-02-10 15:05:52 +0530

    command line calculator all tasks

---
+++ task2/calculator.php
@@ -0,0 +1,42 @@
+<?php
+
+class Calculator
+{
+    public function Add($numbers = 0)
+    {
+        $result = 0;
+        $errors = array();
+        $numbers = explode(',', $numbers);
+        if(!empty($numbers)){
+            foreach ($numbers as $key => $number) {
+                if(is_numeric($number)){
+                    $result += $number;
+                }else{
+                    $errors[] = $number;
+                }
+            }
+        }
+        echo $result."\n";
+        if(!empty($errors)){
+            if(count($errors) == 1){
+                echo "Error : ".implode(',', $errors)." is not a number\n";
+            }else{
+                echo "Error : ".implode(',', $errors)." are not numbers\n";
+            }
+        }
+    }
+}
+
+
+// position [0] is the script's file name
+array_shift($argv);
+$className = 'Calculator';
+$funcName = array_shift($argv);
+if(empty($funcName)){
+    echo "Please enter operation\n";
+    exit();
+}
+echo "Calling '$className::$funcName'...\n";
+call_user_func_array(array($className, $funcName), $argv);
+
+?>
---
+++ task7/calculator.php
@@ -0,0 +1,57 @@
+<?php
+
+class Calculator
+{
+    public function Add($numbers = 0)
+    {
+        $input = explode('\\',$numbers);
+        $delimeter = $input[2];
+        $numbers = preg_split('/'.$delimeter.'/', $input[4] );
+        $result = 0;
+        $errors = array();
+        $negative_numbers = array();
+        if(!empty($numbers)){
+            foreach ($numbers as $key => $number) {
+                if(is_numeric($number)){
+                    if($number < 0){
+                        $negative_numbers[] = $number;
+                    }
+
+                    if($number <= 1000){
+                        $result += $number;
+                    }
+
+                }else{
+                    $errors[] = $number;
+                }
+            }
+        }
+
+        if(!empty($negative_numbers)){
+            echo "Negative numbers {".implode(',', $negative_numbers)."} are not allowed.\n";
+            exit();
+        }
+
+        echo $result."\n";
+        if(!empty($errors)){
+            if(count($errors) == 1){
+                echo "Error : ".implode(',', $errors)." is not a number\n";
+            }else{
+                echo "Error : ".implode(',', $errors)." are not numbers\n";
+            }
+        }
+    }
+}
+
+
+// position [0] is the script's file name
+array_shift($argv);
+$className = 'Calculator';
+$funcName = array_shift($argv);
+if(empty($funcName)){
+    echo "Please enter operation\n";
+    exit();
+}
+echo "Calling '$className::$funcName'...\n";
+call_user_func_array(array($className, $funcName), $argv);
+?>
